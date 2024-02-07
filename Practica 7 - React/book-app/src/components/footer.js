bcfec0070000943a0dd457009b7ba668fb678770
Veronika Tryhubyak <vtr0003@alu.medac.es>
2024-02-07 13:10:42 +0100

    [Book App] - EJERCICIO 1 Terminando

--- Practica 7 - React/book-app/src/components/footer.js
+++ Practica 7 - React/book-app/src/components/footer.js
@@ -4,11 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function Footer() {
   return (
     <div className="container-100">
-      <footer className="d-flex flex-wrap justify-content-between py-3 my-4 border-top bg-dark">
-        <div className="col-md-4 d-flex align-items-center justify-content-center px-3">
+      <footer className="py-3 my-4 border-top bg-dark">
+        <div className="col-12 col-md-6 col-lg-4 d-flex align-items-center justify-content-center px-3">
           <span className="mb-3 mb-md-0 text-light">Veronika Tryhubyak, 2ºDAW </span>
           <a href="https://github.com/tryhubyak" className="m-2 text-light">Visita mi GitHub</a>
-        </div>
+        </div> 
       </footer>
     </div>
   );
